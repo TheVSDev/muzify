c3b14ac270fd1ca08fa2e0db5bddc653677b8178
TheVSDev <vspasic03@gmail.com>
2023-06-30 23:02:32 +0200

    done: added logo

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,6 +1,7 @@
 // Imports
-import HeadPage from "@/web/components/Head"
+import Image from "next/image"
 
+import HeadPage from "@/web/components/Head"
 import styles from "@/web/styles/Welcome.module.css"
 import Footer from "@/web/components/Footer"
 import Main from "@/web/components/Main"
@@ -13,6 +14,7 @@ export default function Welcome() {
             <HeadPage />
 
             <Main>
+                <Image src="/Muzify.png" alt="Muzify logo" width={200} height={58} className={styles.logo} />
                 <div className={styles.container}>
                     <h1 className={styles.title}>Hello ! Welcome to <span className={styles.spanned}>MUZIFY</span></h1>
                     <p className={styles.paragraphe}>Muzify is an online music player.</p>
