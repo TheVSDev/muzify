6c7bb1433811a5affa3499a3edef17794030f74a
TheVSDev <vspasic03@gmail.com>
2023-06-30 21:35:24 +0200

    done: creating welcome page and Button, Head, Main, Footer component

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,6 +2,9 @@
 import HeadPage from "@/web/components/Head"
 
 import styles from "@/web/styles/Welcome.module.css"
+import Footer from "@/web/components/Footer"
+import Main from "@/web/components/Main"
+import Button from "@/web/components/Button"
 
 // Welcome function
 export default function Welcome() {
@@ -9,8 +12,16 @@ export default function Welcome() {
         <>
             <HeadPage />
 
-            <h1 className={styles.title}>Hello ! Welcome to MUZIFY</h1>
-            <p className={styles.paragraphe}>Muzify is an online music player.</p>
+            <Main>
+                <div className={styles.container}>
+                    <h1 className={styles.title}>Hello ! Welcome to <span className={styles.spanned}>MUZIFY</span></h1>
+                    <p className={styles.paragraphe}>Muzify is an online music player.</p>
+                    <br />
+                    <Button btnLabel="Listen to some music" />
+                </div>
+            </Main>
+
+            <Footer />
         </>
     )
 }
