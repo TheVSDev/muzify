0a02fd4b685b7737750af80533c1edfc12651334
TheVSDev <vspasic03@gmail.com>
2023-07-01 23:20:04 +0200

    update: font throughout the site

--- src/web/components/Sidebar.jsx
+++ src/web/components/Sidebar.jsx
@@ -3,24 +3,27 @@ import Link from "next/link";
 
 import styles from "@/web/styles/Sidebar.module.css";
 import HeadPage from "@/web/components/Head";
+import Main from "./Main";
 
 // Sidebar component
 const Sidebar = () => {
   return (
     <>
       <HeadPage />
-      <div className={styles.sidebarContainer}>
-        <div className={styles.linksContainer}>
-          <ul>
-            <li className={styles.sidebarLi}>
-              <Link className="" href="/">
-                Home
-              </Link>
-            </li>
-            <li className={styles.sidebarLi}>Search</li>
-          </ul>
+      <Main>
+        <div className={styles.sidebarContainer}>
+          <div className={styles.linksContainer}>
+            <ul>
+              <li className={styles.sidebarLi}>
+                <Link className="" href="/">
+                  Home
+                </Link>
+              </li>
+              <li className={styles.sidebarLi}>Search</li>
+            </ul>
+          </div>
         </div>
-      </div>
+      </Main>
     </>
   );
 };
