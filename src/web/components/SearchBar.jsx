233ebb70596695c84f981bfaf449a270afb2bc3e
TheVSDev <vspasic03@gmail.com>
2023-07-02 21:34:33 +0200

    done: search bar

--- src/web/components/SearchBar.jsx
+++ src/web/components/SearchBar.jsx
@@ -1,11 +1,19 @@
-import styles from "@/web/styles/Search.module.css"
+import Image from "next/image";
+import styles from "@/web/styles/Search.module.css";
 
 const SearchBar = () => {
-    return (
-        <>
-            <input type="text" className={styles.searchBar} />
-        </>
-    )
-}
+  return (
+    <>
+      <Image
+        src="/search.svg"
+        alt="Search icon"
+        width={25}
+        height={58}
+        className={styles.icon}
+      />
+      <input type="text" className={styles.searchBar} />
+    </>
+  );
+};
 
-export default SearchBar
+export default SearchBar;
