66983029ba5eb7f8399b9e84391be9ab6c5902f6
TheVSDev <vspasic03@gmail.com>
2023-07-09 23:41:01 +0200

    done: upload button working, displaying and playing the uploaded file

--- src/pages/upload.jsx
+++ src/pages/upload.jsx
@@ -1,12 +1,28 @@
 // Imports
+import React, { useState } from "react";
+
 import HeadPage from "@/web/components/Head";
 import Footer from "@/web/components/Footer";
 import Main from "@/web/components/Main";
 import Sidebar from "@/web/components/Sidebar";
-import styles from "@/web/styles/Upload.module.css"
+import styles from "@/web/styles/Upload.module.css";
 
 // Welcome function
 export default function Upload() {
+  const [audioFile, setAudioFile] = useState({ file: null, name: "" });
+
+  const handleFileUpload = (event) => {
+    const uploadedFile = event.target.files[0];
+    setAudioFile({
+      file: URL.createObjectURL(uploadedFile),
+      name: uploadedFile.name,
+    });
+  };
+
+  const handleDeleteFile = () => {
+    setAudioFile(null);
+  };
+
   return (
     <>
       <HeadPage />
@@ -14,8 +30,31 @@ export default function Upload() {
       <Main>
         <Sidebar />
         <div className={styles.container}>
-            <input type="file" accept="audio/*" name="upload_music" id="file" className={styles.upload} />
-            <label for="file" className={styles.label}>Upload music</label>
+          <input
+            type="file"
+            accept="audio/*"
+            onChange={handleFileUpload}
+            name="upload_music"
+            id="file"
+            className={styles.upload}
+          />
+          <label for="file" className={styles.label}>
+            Upload music
+          </label>
+          <button onClick={handleDeleteFile} className={styles.btnCtrl}>
+            Delete
+          </button>
+        </div>
+        <div className={styles.musicContainer}>
+          {audioFile && (
+            <div>
+              <p>{audioFile.name}</p>
+              <audio controls className={styles.audioPlayer}>
+                <source src={audioFile.file} type="audio/mpeg" />
+                Your browser does not support the audio element.
+              </audio>
+            </div>
+          )}
         </div>
       </Main>
 
